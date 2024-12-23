be901319d6451be39b50d1042a3a8db4d9bf3b79
Christopher Brown <cbrown@muskegonisd.org>
2024-12-22 19:40:35 -0500

    Added Week 17 Item code.

---
+++ Armor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS0402_RPGFramework
+{
+    public class Armor : Item
+    {
+        // TODO
+        // Add armor properties (defense, speed, etc.)
+        // Implement armor methods
+        // Need to think about how combat is going to work first
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS0402_RPGFramework
+{
+    public class Weapon : Item
+    {
+        public double Damage { get; set; } = 0;
+
+        // TODO
+        // Add attack properties (damage, speed, etc.)
+        // Implement attack methods
+        // Maybe some kind of Weapon generator (random stats, etc.)
+    }
+}
